11218ca6322dbed1d3ed98aaca9d894579bf1f56
Naresh kumar Vadakatta <vnareshkumar111@gmail.com>
2017-11-05 21:15:28 +0530

    Added files

---
+++ src/main/java/com/techsnob/ReviewBoard/ReviewBoardApplication.java
@@ -0,0 +1,12 @@
+package com.techsnob.ReviewBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewBoardApplication.class, args);
+	}
+}

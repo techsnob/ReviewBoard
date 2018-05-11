0a77068104906960d03cafb58025cf1699d11557
naresh <vnareshkumar111@gmail.com>
2018-05-11 19:49:32 +0530

    Working condition without spring security

--- src/main/java/com/techsnob/configuration/WebMvc.java
+++ src/main/java/com/techsnob/configuration/WebMvc.java
@@ -1,54 +1,16 @@
 package com.techsnob.configuration;
 
-import javax.persistence.EntityManagerFactory;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableJpaAuditing
-public class WebMvc {
+public class WebMvc /*implements WebMvcConfigurer */{
 
-	@Bean
+	/*@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		return bCryptPasswordEncoder;
-	}
+	}*/
 	
-	/*@Bean
-	  public AuditorAware<AuditableUser> auditorProvider() {
-	    return new AuditorAwareImpl();
-	  }
 	
-	@Bean
-	  public EntityManagerFactory entityManagerFactory() {
-
-	    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-	    vendorAdapter.setGenerateDdl(true);
-
-	    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-	    factory.setJpaVendorAdapter(vendorAdapter);
-	    factory.setPackagesToScan("com.acme.domain");
-	    factory.setDataSource(dataSource());
-	    factory.afterPropertiesSet();
-
-	    return factory.getObject();
-	  }
-
-	
-	@Bean
-	  public PlatformTransactionManager transactionManager() {
-
-	    JpaTransactionManager txManager = new JpaTransactionManager();
-	    txManager.setEntityManagerFactory(entityManagerFactory());
-	    return txManager;
-	  }*/
-
 }
--- src/main/java/com/techsnob/service/UserServiceImpl.java
+++ src/main/java/com/techsnob/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.techsnob.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.techsnob.entities.User;
@@ -14,8 +13,8 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
     
     @Override
 	public User findUserByName(String username) throws UserNotFoundByNameException {
@@ -29,7 +28,7 @@ public class UserServiceImpl implements UserService {
     
     @Override
 	public void saveUser(User user) {
-		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		user.setEnabled(false);
 		userRepository.save(user);
 	}
